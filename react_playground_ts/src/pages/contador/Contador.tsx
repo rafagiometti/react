2a051c7351d0b2da7936b3f80c6343b87082a206
rafaela_giometti <rafaela_giometti@hotmail.com>
2025-06-17 10:48:40 -0300

    :tada: Commit inicial com estrutura base do projeto React + TypeScript

---
+++ react_playground_ts/src/pages/contador/Contador.tsx
@@ -0,0 +1,26 @@
+﻿import { useState } from "react"
+ 
+function Contador() {
+ 
+  const[valor, setValor] = useState(0);
+ 
+  function handleClick() {
+    setValor(valor + 1);
+  }
+
+  function handleClickSubtrair(){
+    setValor(valor - 1);
+  }
+ 
+  return (
+    <div>
+      <h1>Componente Contador</h1>
+      <p>O valor atual é: {valor}</p>
+      <button onClick={handleClick}>Adicionar 1</button>
+      <button onClick={handleClickSubtrair}>Subtrair 1</button>
+      </div>
+  )
+}
+ 
+export default Contador
+ 
