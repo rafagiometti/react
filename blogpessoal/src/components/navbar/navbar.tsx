61b7c06fc95e140d0b96ce75c9a7272cd22bc304
rafaela_giometti <rafaela_giometti@hotmail.com>
2025-06-24 15:50:59 -0300

    :sparkles: Atualizando componentes

--- blogpessoal/src/components/navbar/navbar.tsx
+++ blogpessoal/src/components/navbar/navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
             			   bg-indigo-900 text-white'>
             
                 <div className="container flex justify-between text-lg">
-                    Blog Pessoal
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
                     <div className='flex gap-4'>
                         Postagens
