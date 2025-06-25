80ca74995cc4bee4576a0b9cb0e1e19757c0f62b
rafaela_giometti <rafaela_giometti@hotmail.com>
2025-06-25 12:12:58 -0300

    :sparkles: Renomeando componentes

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -1,58 +1,24 @@
-import {
-	FacebookLogoIcon,
-	InstagramLogoIcon,
-	LinkedinLogoIcon,
-} from '@phosphor-icons/react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import './App.css'
 
-function Footer() {
-	const data = new Date().getFullYear()
-
-	return (
-		<>
-			<div className="flex justify-center bg-indigo-900 text-white">
-				<div className="container flex flex-col items-center py-4">
-					<p className="text-xl font-bold">
-						Blog Pessoal Generation | Copyright:{' '}
-						{data}
-					</p>
-					<p className="text-lg">
-						Acesse nossas redes sociais
-					</p>
-					<div className="flex gap-2">
-						<div className="flex gap-2">
-							<a
-								href="https://www.linkedin.com/in/felipe-peronica/"
-								target="_blank"
-							>
-								<LinkedinLogoIcon
-									size={48}
-									weight="bold"
-								/>
-							</a>
-							<a
-								href="https://www.instagram.com/seu_usuario"
-								target="_blank"
-							>
-								<InstagramLogoIcon
-									size={48}
-									weight="bold"
-								/>
-							</a>
-							<a
-								href="https://www.facebook.com/seu_usuario"
-								target="_blank"
-							>
-								<FacebookLogoIcon
-									size={48}
-									weight="bold"
-								/>
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</>
-	)
+function App() {
+  return (
+    <>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+  )
 }
 
-export default Footer
+export default App
